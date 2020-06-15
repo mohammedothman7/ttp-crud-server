8d1972607169ccffe25084d3476dc4a060b86da4
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-06-15 03:28:02 -0400

    chore: added images to campuses seed file

--- utils/seedDatabase.js
+++ utils/seedDatabase.js
@@ -6,16 +6,22 @@ const seedDatabase = async () => {
       name: 'Brooklyn College',
       address: 'Brooklyn',
       description: 'A college in Brooklyn',
+      imageUrl:
+        'https://petersonsbackup.blob.core.windows.net/static/cdn/profiles/10000623/e042198d-406b-4f83-b5a3-5d0adb2fd2c5',
     }),
     Campus.create({
       name: 'College of Staten Island',
       address: 'Staten Island',
       description: 'A college on Staten Island',
+      imageUrl:
+        'https://zenprospect-production.s3.amazonaws.com/uploads/logos/559234c473696418f681cb00/logo',
     }),
     Campus.create({
       name: 'John Jay College',
       address: 'Manhattan',
       description: 'A college in Manhattan',
+      imageUrl:
+        'https://www.jjay.cuny.edu//sites/default/files/marketing_development/jj_logo_color.png',
     }),
     Student.create({
       firstName: 'Daniel',

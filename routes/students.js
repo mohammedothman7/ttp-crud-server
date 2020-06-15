2bae64852d55f8d8162721ed7e6e1562324814d0
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-06-14 22:30:08 -0400

    feat: added functions to get a specific user, delete, put, and add a user to db

--- routes/students.js
+++ routes/students.js
@@ -1,10 +1,10 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-const { Student } = require("../database/models");
+const { Student } = require('../database/models');
 
 /* GET all students. */
 // /api/students
-router.get("/", async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   // try to get students object from database
   try {
     // students will be the result of the Campus.findAll promise
@@ -18,10 +18,69 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.put("/:id", async (req, res, next) => {
+// Route to getting one student
+router.get('/:id', async (req, res, next) => {
+  // take the id from params
   const { id } = req.params;
-  const { campusId } = req.body;
-  const updatedObj = { campusId: campusId };
+  // query the database for a student with matching id
+  try {
+    // if successful:
+    const student = await Student.findByPk(id);
+    // send back the student as a response
+    res.status(200).json(student);
+  } catch (err) {
+    // if error:
+    // handle error
+    next(err);
+  }
+});
+
+// Route to handle adding a student
+// /api/students/
+router.post('/', async (req, res, next) => {
+  // Take the form data from the request body
+  const { firstName, lastName, email, gpa, imageUrl } = req.body;
+  // Create a student object
+  const studentObj = {
+    firstName,
+    lastName,
+    email,
+    gpa,
+    imageUrl,
+  };
+  try {
+    // Create a new student on the database
+    const newStudent = await Student.create(studentObj);
+    // The database would return a student
+    // send that student as a json to the client
+    res.status(201).send(newStudent);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Route to handle removing a student
+router.delete('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  // get an id for a student to delete
+  try {
+    // pass the id to the database to find student to be deleted
+    // database would either respond succcess or fail
+    const student = await Student.findByPk(id);
+    // invoke the .destroy() method on the returned student
+    await student.destroy();
+    // send a success message to the client
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Route to edit a student
+router.put('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const { firstName, lastName, email, gpa, imageUrl } = req.body;
+  const updatedObj = { firstName, lastName, email, gpa, imageUrl };
   try {
     const student = await Student.findByPk(id);
     await student.set(updatedObj);

5b4958d7e0f9037b7860fd1a6b2795e08061df01
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-06-14 22:29:21 -0400

    chore: reformated file

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
 // Subrouters;
-const campusesRouter = require("./campuses");
-const studentsRouter = require("./students");
+const campusesRouter = require('./campuses');
+const studentsRouter = require('./students');
 
 // Mount our subrouters to assemble our apiRouter;
 
-router.use("/campuses", campusesRouter);
-router.use("/students", studentsRouter);
+router.use('/campuses', campusesRouter);
+router.use('/students', studentsRouter);
 
 // Error handling middleware;
 router.use((req, res, next) => {
-  const error = new Error("Not Found, Please Check URL!");
+  const error = new Error('Not Found, Please Check URL!');
   error.status = 404;
   next(error);
 });

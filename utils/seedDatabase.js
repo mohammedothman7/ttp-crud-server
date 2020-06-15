9276dc874186d2bb59c19b948a74d5f0831489b7
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-06-14 22:30:29 -0400

    added more students to seed file

--- utils/seedDatabase.js
+++ utils/seedDatabase.js
@@ -1,24 +1,35 @@
-const { Campus, Student } = require("../database/models");
+const { Campus, Student } = require('../database/models');
 
 const seedDatabase = async () => {
   await Promise.all([
     Campus.create({
-      name: "Brooklyn College",
-      address: "Brooklyn",
-      description: "A college in Brooklyn",
+      name: 'Brooklyn College',
+      address: 'Brooklyn',
+      description: 'A college in Brooklyn',
     }),
     Campus.create({
-      name: "College of Staten Island",
-      address: "Staten Island",
-      description: "A college on Staten Island",
+      name: 'College of Staten Island',
+      address: 'Staten Island',
+      description: 'A college on Staten Island',
     }),
     Campus.create({
-      name: "John Jay College",
-      address: "Manhattan",
-      description: "A college in Manhattan",
+      name: 'John Jay College',
+      address: 'Manhattan',
+      description: 'A college in Manhattan',
     }),
-    Student.create({ firstName: "Daniel" }),
-    Student.create({ firstName: "Sally", campusId: 1 }),
+    Student.create({
+      firstName: 'Daniel',
+      lastName: 'Smith',
+      email: 'DSmith@example.com',
+      gpa: 3.9,
+    }),
+    Student.create({
+      firstName: 'Jon',
+      lastName: 'Snow',
+      email: '4thewatch@example.com',
+      gpa: 2.0,
+      campusId: 1,
+    }),
   ]);
 };
 
